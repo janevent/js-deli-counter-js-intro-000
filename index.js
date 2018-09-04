b643517337ae6fa9f26f7fb0b354489b5a807414
Jane Venter <janeanneventer@gmail.com>
2018-09-04 13:34:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,13 +10,7 @@ function nowServing(line){
     return "There is nobody waiting to be served!";
   }
 }
- 1) deli currentLine(line) says who is in line when there are people waiting:
-
-      Error: Expected 'The line is currently: 1. Bill' to equal 'The line is currently: 1. Bill, 2. Jane, 3. Ann'
-      + expected - actual
-
-      -The line is currently: 1. Bill
-      +The line is currently: 1. Bill, 2. Jane, 3. Ann
+ 
 
 
 
