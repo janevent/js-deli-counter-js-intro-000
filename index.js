66f231e04e79b3561eea6560226221b514878aa6
Jane Venter <janeanneventer@gmail.com>
2018-09-04 13:50:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,8 +13,9 @@ function nowServing(line){
 function currentLine(line){
   if(line.length > 0){
     var text = "The line is currently:";
+    var newArray = [];
     for(let i = 0; i > line.length; i ++){
-      return ` ${text} ${i + 1} ${line[i]}`;
+      push.newArray(` ${text} ${i + 1} ${line[i]}`);
     }
     }else if(line.length === 0){
       return "The line is currently empty.";
